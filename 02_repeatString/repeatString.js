304188669643affd49def8763db2744a58448c01
avearmin <arminradoncic1@gmail.com>
2023-02-21 17:10:19 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            newString += string;
+        }
+    } else {
+        return "ERROR";
+    }
+    return newString;
 
 };
 
