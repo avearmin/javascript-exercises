f69f11354a7307e6ef8edc09606bda3140ee6a6a
avearmin <arminradoncic1@gmail.com>
2023-02-24 14:50:42 -0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedString = string.split(/[\W+_+]/).join("")
+    let reversedString = cleanedString.split("").reverse().join("");
+    return cleanedString.toLowerCase() === reversedString.toLowerCase();
 };
-
 // Do not edit below this line
 module.exports = palindromes;
