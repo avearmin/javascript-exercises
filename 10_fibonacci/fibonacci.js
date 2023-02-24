ca3560e72cad5e25288003ad6f8e2432751edc1d
avearmin <arminradoncic1@gmail.com>
2023-02-24 15:48:22 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let fibonacciArray = [1, 1];
+    if (position < 0) return "OOPS";
+    else {
+        for (i = 2; i < position; i++) {
+            fibonacciArray.push(fibonacciArray[i-1] + fibonacciArray[i-2]);
+        }
+    return fibonacciArray[fibonacciArray.length - 1];
+    }
 };
-
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
