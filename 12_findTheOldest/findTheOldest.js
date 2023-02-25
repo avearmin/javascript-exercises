36895d0e002cd04ca697917eb0f03d2762ae522e
avearmin <arminradoncic1@gmail.com>
2023-02-24 22:25:24 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestPerson;
+    let oldestAge = 0;
+    const currentYear = new Date().getFullYear();
+    
+    for (item of array) {
+        if (item.hasOwnProperty("yearOfDeath")) {
+            if (item.yearOfDeath - item.yearOfBirth > oldestAge) {
+                oldestAge = item.yearOfDeath - item.yearOfBirth;
+                oldestPerson = item;
+            }
+        } else {
+            if (currentYear - item.yearOfBirth > oldestAge) {
+                oldestAge = currentYear - item.yearOfBirth;
+                oldestPerson = item;
+            }
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
